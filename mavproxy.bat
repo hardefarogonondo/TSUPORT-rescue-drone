b9abdbc0dfe2adba2e3ee8806d3bcfa68cf88a88
Hardefa Rogonondo <hardefarizukypr@gmail.com>
2020-08-08 17:43:14 +0700

    deleted

--- mavproxy.bat
+++
@@ -1,5 +0,0 @@
-cd C:/Python27/Scripts
-python mavproxy.py --master tcp:192.168.0.99:6789 baudrate 57600 --out 127.0.0.1:14550 --out 127.0.0.1:14551
-cmd.exe
-
-python mavproxy.py --master /dev/ttyUSB0 --baudrate 57600 --out 127.0.0.1:14550 --out 127.0.0.1:14551
