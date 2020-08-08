1c9195464b913c57641cd94853c472808fe6bf97
Hardefa Rogonondo <hardefarizukypr@gmail.com>
2020-08-08 17:04:33 +0700

    Robot Calibration Program

--- Calibrate.py
+++ Calibrate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-© Copyright 2020, TSUPORT (Tsunami Post Disaster Robot) Research Team.
+© Copyright 2020, TSUPORT (Tsunami Post Disaster Robot) Research Team, Politeknik Elektronika Negeri Surabaya.
 By: Hardefa Rizky Putu Rogonondo (Mechatronics Engineering 2016, 3110161019).
 """
 
@@ -11,9 +11,9 @@ import json
 import time
 
 # Initialization
-# Connection string for Ubuntu = /dev/ttyUSB0 | Windows = COMXX | MAVProxy = 127.0.0.1:14550.
-connection_string =  "COM10" #"/dev/ttyUSB0"
-gps_save = {} # Create empty GPS coordinate dictionary.
+# Connection string for Ubuntu = /dev/ttyUSB0 | Windows = COMXX | MAVProxy = 127.0.0.1:14551.
+connection_string =  "127.0.0.1:14551"
+gps_save = {} # Create an empty GPS coordinate dictionary.
 gps_save["latitude_longitude"] = [] # Insert the dictionary with latitude_longitude variable.
 
 # Opening Messages
@@ -23,7 +23,8 @@ print("\n\n---Welcome to TSUPORT Waypoint Calibration Program---\n\n")
 print("Connecting to vehicle on: %s" %connection_string)
 vehicle = connect(connection_string, wait_ready = False, heartbeat_timeout = 100, baud = 57600) # wait_ready = False is used to bypass the paramters loading and connecting the vehicle first.
 vehicle.wait_ready(True, timeout = 150) # wait_ready = True is to load parameters now.
-while vehicle.gps_0.fix_type < 6 and vehicle.gps_0.eph <= 200: #and vehicle.gps_0.epv < 200:
+#while vehicle.gps_0.fix_type < 6 and vehicle.gps_0.eph <= 200: #and vehicle.gps_0.epv < 200:
+while vehicle.gps_0.satellites_visible < 6 and vehicle.gps_0.eph <= 200:
 # EPH = HDOP * 100 | Ideal HDOP <= 1 | Excellent HDOP <= 2 | Good HDOP <= 5 | Moderate HDOP <= 10.
     print "Waiting for GPS...:", vehicle.gps_0.fix_type
     time.sleep(1)
